69baf1bba2113c7d6977838581334b775b752315
Metzo444 <91372010+Metzo444@users.noreply.github.com>
2021-11-16 14:00:17 +0400

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from to_do.models import Film
+
+admin.site.register(Film)
--- views.py
+++ views.py
@@ -1,36 +1,35 @@
-# from django.shortcuts import render
 from django.shortcuts import HttpResponse
-from datetime import datetime
 
-# Create your views here.
-def home(request):
-    print(request.__dict__)
-    return HttpResponse('Hello from Django app ')
+from to_do.models import Film
+
+# from to_do.models import Task
+
+film_d = Film.objects.get(id=1)
+film_d.delete()
+
+film_2 = Film(name='Harry Potter', rate=5, is_punlished=True, status=2)
+film_2.save()
 
 
-# def home_1(request):
-#     print(request.__dict__)
-#     return HttpResponse('Hello from Django app Aram')
+def home_1(request):
+    films = Film.objects.all()
 
-def present(request):
+    return HttpResponse('hello there {} {}'.format(films[1], films[2]))
 
-    return HttpResponse("Aram Azatyan")
 
-def greeting(request):
-    return HttpResponse('Welcome to Python Django')
+def film(request):
+    film_4 = Film(name='Home Alone', rate=5, is_punlished=True, status=2)
+    film_4.save()
+    films = Film.objects.all()
+    return HttpResponse('Hello python {} {} {}'.format(films[0], films[1], films[2]))
 
-def introduction(request):
 
-    return HttpResponse('''Django (web framework)
-    Django is a Python-based free and open-source web framework that follows the model–template–views (MTV) architectural pattern.
-    It is maintained by the Django Software Foundation (DSF), an independent organization established in the US as a 501(c)(3) non-profit.
-    Django's primary goal is to ease the creation of complex, database-driven websites. The framework emphasizes reusability and "pluggability" of components, less code, low coupling, rapid development, and the principle of don't repeat yourself.[9] Python is used throughout, even for settings, files, and data models. Django also provides an optional administrative create, read, update and delete interface that is generated dynamically through introspection and configured via admin models.
-    ''')
+def help_film(request):
+    try:
+        films = Film.objects.get(id=1, name='Forrest Gump', rate=9)
+    except Film.DoesNotExist:
+        films = None
+        films = Film.objects.filter(name='Home alone', rate=5)
+        print(films.query)
+        return HttpResponse(films)
 
-def time(request):
-    current_datetime = datetime.now()
-    return HttpResponse(current_datetime)
-
-def solution_task(request):
-    a = [{x:x**2 for x in range(1,16)}]
-    return HttpResponse(a)
